302055073db97a4ddf5eb0430f2eba1d81695012
ikara-py <ali16kara@gmail.com>
2026-03-27 11:10:24 +0100

    feat: Refactor account creation logic to improve validation and structure

--- app/Services/AccountServices.php
+++ app/Services/AccountServices.php
@@ -26,32 +26,36 @@ class AccountServices{
 
             $guardian = $this->userRepository->findOrFail($data['guardian_id']);
 
-            if ($data['type'] === 'MINEUR' && !$creator->isMinor()){
+            if (!$creator->isMinor()){
                 throw ValidationException::withMessages(['type' => ['the creator can not be a minor']]);
             }
-
-            $accountData = [
-                'account_number' => $this->generateAccountNumber(),
-                'type' => $data['type'],
-                'status' => 'ACTIVE',
-                'balance' => 0.00,
-                'guardian_id' => $data['guardian_id'],
-            ];
-
-            match ($data['type']){
-                'COURANT' => $accountData +=[
-                    'overdraft_limit' => $data['overdraft_limit'] ?? config('banking.overdraft_limit'),
-                    'monthly_fee' => $data['monthly_fee'] ?? config('banking.monthly_fee'),
-                ],
-                'EPARGNE' => $accountData +=[
-                    'interest_rate' => $data['interest_rate'] ?? config('banking.savings_interest_rate'),
-                ],
-                'MINEUR' => $accountData +=[
-                    'interest_rate' => $data['interest_rate'] ?? config('banking.minor_interest_rate'),
-                ],
-                default => throw new \Exception("Invalid account type"),
-            };
-
         }
+
+        $accountData = [
+            'account_number' => $this->generateAccountNumber(),
+            'type' => $data['type'],
+            'status' => 'ACTIVE',
+            'balance' => 0.00,
+            'guardian_id' => $data['guardian_id'] ?? null,
+        ];
+
+        match ($data['type']){
+            'COURANT' => $accountData +=[
+                'overdraft_limit' => $data['overdraft_limit'] ?? config('banking.overdraft_limit'),
+                'monthly_fee' => $data['monthly_fee'] ?? config('banking.monthly_fee'),
+            ],
+            'EPARGNE' => $accountData +=[
+                'intrest_rate' => $data['interest_rate'] ?? config('banking.savings_interest_rate'),
+            ],
+            'MINEUR' => $accountData +=[
+                'intrest_rate' => $data['interest_rate'] ?? config('banking.minor_interest_rate'),
+            ],
+            default => throw new \Exception("Invalid account type"),
+        };
+
+        $account = $this->accountRepository->create($accountData);
+        $this->accountRepository->attachUser($account, $creator->id, 'PROPRIETAIRE');
+
+        return $account;
     }
 }
